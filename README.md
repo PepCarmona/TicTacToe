92a7abe009e110b7193e14808299c597e6a914db
PepCarmona <pep.carmona.coll@gmail.com>
2020-10-14 19:19:10 +0200

    Click and player turn mechanics
    Added click events depending on active player (show "X" or "O" in cells). Added dataset (player and clicked) to cells and 'moves' variable in order to control further game flow.

--- README.md
+++ README.md
@@ -9,7 +9,23 @@ At this point the project runs without any dependencies and almost on every web 
 
 Features and changes are listed below:
 
+Changelog v0.0.2 [14/10/2020] {
+
+    - player and click mechanics -  
+
+    ·   added 'playerStatus' class div to show player information
+    ·   added 'move' variable to store the number of moves made in total
+    ·   added 'activePlayer' variable to store the current player
+    ·   added 'clickCell()' event listener to every cell on creation
+    ·   added player dataset to "cell" class divs
+    ·   added clicked dataset functionality - show message when clicking on an already clikced cell
+    ·   show "X" or "O" depending on the player that clicked
+    ·   show current player and number of moves in 'playerStatus' div
+}
 Changelog v0.0.1 [14/10/2020] {
+
+    - grid visual representation - 
+
     ·   game board dynamic representation
     ·   selectable game board dimensions
     ·   game board positioning
--- sourceCode/X-Line.js
+++ sourceCode/X-Line.js
@@ -1,7 +1,10 @@
 /* variables are declared in order to access to different sections of the html more easily */
 let container = document.querySelector(".container");
+let playerStatus = document.querySelector(".playerStatus");
+var moves = 0;
 var dimension = 0;
 var boardLength = 0;
+var activePlayer = 1;
 
 /* _____GAME BOARD SETTINGS______ */
 /* activeBoard() function will get the properties of the selected game board */
@@ -31,6 +34,7 @@ activeBoard(document.getElementById("dimensions"));
         node.dataset.col = col;
         node.dataset.index = i;
         node.dataset.clicked = false;
+        node.addEventListener("click", function() {clickCell(this);});
         node.innerHTML = i;
         container.appendChild(node);
         col += 1;
@@ -42,4 +46,38 @@ activeBoard(document.getElementById("dimensions"));
     // this script sets the grid style depending on the selected game board
     container.style.width = (dimension * 121)+"px";
     container.style.gridTemplateColumns = "repeat("+dimension+", auto)";
+}
+
+/* ______CLICK EVENTS______ */
+/* clickCell(element) function groups the actions that take place whenever the user clicks a cell */
+
+function clickCell(element){
+    if (element.dataset.clicked == "false"){
+        clickPlayer(element);
+    } else clickedAnimation();
+}
+
+/* clickPlayer(element) function checks which player clicked the cell and marks the cell depending on it */
+
+function clickPlayer(element){
+    if (activePlayer == 1){
+        element.innerHTML = "O";
+        element.dataset.player = 1;
+        activePlayer = 2;
+        playerStatus.innerHTML = "ACTIVE : 2";
+    } else if (activePlayer == 2){
+        element.innerHTML = "X";
+        element.dataset.player = 2;
+        activePlayer = 1;
+        playerStatus.innerHTML = "ACTIVE : 1";
+    }
+    element.dataset.clicked = true;
+    moves += 1;
+    playerStatus.innerHTML += " | MOVES : "+moves;
+}
+
+/* clickedAnimation(element) function shows a message to notify the user that this cell is not available 
+   for clicking on it again */
+function clickedAnimation(){
+    alert("This cell has been already clicked.");
 }
