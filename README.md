89f6247f848df90500097325c4ddf79d05962fb6
PepCarmona <pep.carmona.coll@gmail.com>
2020-10-14 20:19:39 +0200

    win conditions
    Added a bunch of checking functions in order to verify the player who clicked on every cell and evaluate if all cells in a row/column/diagonal are clicked by the same player

--- README.md
+++ README.md
@@ -9,6 +9,22 @@ At this point the project runs without any dependencies and almost on every web 
 
 Features and changes are listed below:
 
+Changelog v0.0.3 [14/10/2020] {
+
+    - win conditions and game over -
+
+    ·   added 'check()' function that groups different wining conditions evaluation subfunctions
+    ·   added 'checkRow()' function to check win condition in a clicked row
+    ·   added 'checkColumn()' function to check win conditions in a clicked column
+    ·   added 'setDiagonals()' function to find cells that form the main and secondary diagonals of the grid 
+        (stored in  'mainDiagonal' and 'secondaryDiagonal' variables)
+    ·   added 'checkMainDiagonal()' function to check win contitions if a cell is in the main diagonal
+    ·   added 'checkSecondaryDiagonal()' function to check win conditions if a cell is in the secondary diagonal
+    ·   added 'gameStatus' class div to show game state
+    ·   cells created in 'showGameBoard()' are now grouped in an iterable 'cells' array in order to filter and check
+    ·   removed console.log instruction in 'activeBoard()' (line 15) used previously to check software flow information
+}
+
 Changelog v0.0.2 [14/10/2020] {
 
     - player and click mechanics -  
@@ -22,6 +38,7 @@ Changelog v0.0.2 [14/10/2020] {
     ·   show "X" or "O" depending on the player that clicked
     ·   show current player and number of moves in 'playerStatus' div
 }
+
 Changelog v0.0.1 [14/10/2020] {
 
     - grid visual representation - 
--- sourceCode/X-Line.js
+++ sourceCode/X-Line.js
@@ -1,10 +1,28 @@
-/* variables are declared in order to access to different sections of the html more easily */
+/* variables and functions are declared in order to ease acces along the document */
 let container = document.querySelector(".container");
 let playerStatus = document.querySelector(".playerStatus");
+let gameStatus = document.querySelector(".gameStatus");
 var moves = 0;
 var dimension = 0;
 var boardLength = 0;
 var activePlayer = 1;
+let cells = [];
+var mainDiagonal = [];
+var secondaryDiagonal = [];
+function setDiagonals(squaredArray){
+    let row1 = 0;
+    let row2 = 0;
+    for (let i=0; i<boardLength; i++){
+        if (squaredArray[i].dataset.row == row1 && squaredArray[i].dataset.col == row1){
+            mainDiagonal.push(squaredArray[i]);
+            row1 += 1;
+        }
+        if (squaredArray[i].dataset.row == row2 && squaredArray[i].dataset.col == (dimension-row2-1)){
+            secondaryDiagonal.push(squaredArray[i]);
+            row2 += 1;
+        }
+    }
+}
 
 /* _____GAME BOARD SETTINGS______ */
 /* activeBoard() function will get the properties of the selected game board */
@@ -12,7 +30,6 @@ function activeBoard(sel){
     let text = sel.options[sel.selectedIndex].text;
     dimension = parseInt(text.substring(0,1));
     boardLength = dimension * dimension;
-    console.log("dimension: "+dimension+". BoardLength: "+boardLength);
     container.innerHTML = "";
     showGameBoard();
 }
@@ -23,9 +40,12 @@ activeBoard(document.getElementById("dimensions"));
 /* showGameBoard() function changes the grid disposition and number of cells depending on the selected
    game board dimensions */
    function showGameBoard(){
-    // initialize local variables and each time a new game board is built
+    // initialize local variables each time a new game board is built
     let row = 0;
     let col = 0;
+    cells = [];
+    diagonalPrincipal = [];
+    diagonalSecundaria = [];
     // this loop creates and appends as many div cell elements as defined by the boardLength variable 
     for (var i=0; i<boardLength; i++){
         var node = document.createElement("div");
@@ -37,6 +57,7 @@ activeBoard(document.getElementById("dimensions"));
         node.addEventListener("click", function() {clickCell(this);});
         node.innerHTML = i;
         container.appendChild(node);
+        cells.push(node);
         col += 1;
         if (col == (dimension)){
             col = 0;
@@ -46,14 +67,19 @@ activeBoard(document.getElementById("dimensions"));
     // this script sets the grid style depending on the selected game board
     container.style.width = (dimension * 121)+"px";
     container.style.gridTemplateColumns = "repeat("+dimension+", auto)";
+    // and calls the setDiagonals() function in order to store the cells that are part of the main and secondary diagonal
+    setDiagonals(cells);
 }
 
 /* ______CLICK EVENTS______ */
 /* clickCell(element) function groups the actions that take place whenever the user clicks a cell */
 
 function clickCell(element){
-    if (element.dataset.clicked == "false"){
+    if (activePlayer == 0){
+        alert("Game is Over")
+    } else if (element.dataset.clicked == "false"){
         clickPlayer(element);
+        check(element);
     } else clickedAnimation();
 }
 
@@ -76,6 +102,52 @@ function clickPlayer(element){
     playerStatus.innerHTML += " | MOVES : "+moves;
 }
 
+/* ______WIN CONDITIONS______ */
+/* check(element) functions verifies if the win conditions are met or if the game result is draw */
+
+function check(element){
+    let rowResult = checkRow(element);
+    let columnResult = checkColumn(element);
+    let mainDiagonalResult = checkMainDiagonal(element);
+    let secondaryDiagonalResult = checkSecondaryDiagonal(element);
+    if (rowResult || columnResult || mainDiagonalResult || secondaryDiagonalResult){
+        gameStatus.innerHTML = "The winner is: player "+element.dataset.player+".";
+        activePlayer = 0; // prevents an infinite game loop
+    } else if (moves == boardLength){
+        gameStatus.innerHTML = "Draw."
+    }
+}
+
+/* checkRow(element) function filters the cells with the same element.dataset.row value and then verifies
+   if every one of them were clicked by the same player */
+   function checkRow(element){
+    let rowCells = cells.filter(cell => cell.dataset.row == element.dataset.row);
+    return rowCells.every(cell => cell.dataset.player == rowCells[0].dataset.player);
+}
+
+/* checkColumn(element) function filters the cells with the same element.dataset.col value and then verifies
+   if every one of them were clicked by the same player */
+   function checkColumn(element){
+    let columnCells = cells.filter(cell => cell.dataset.col == element.dataset.col);
+    return columnCells.every(cell => cell.dataset.player == columnCells[0].dataset.player);
+}
+/* checkDiagonal(element) functions filter the cells in the diagonals of the current board
+   and then verifies if every one of them were clicked by the same player */
+
+   function checkMainDiagonal(element){
+    if (mainDiagonal.includes(element)){
+        return mainDiagonal.every(cell => cell.dataset.player == mainDiagonal[0].dataset.player);
+    }
+    
+}
+function checkSecondaryDiagonal(element){
+    if (secondaryDiagonal.includes(element)){
+        return secondaryDiagonal.every(cell => cell.dataset.player == secondaryDiagonal[0].dataset.player);
+    }
+    
+}
+
+/* ______ALREADY CLICKED______ */
 /* clickedAnimation(element) function shows a message to notify the user that this cell is not available 
    for clicking on it again */
 function clickedAnimation(){
